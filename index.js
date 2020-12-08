836e124389cc47cd619329d74a2f68dc05632334
Devilzer <jenadeepak724@gmail.com>
2020-12-08 20:18:21 +0530

    add mongo database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-
+const db = require("./config/mongoose");
 app.use("/",require("./routes"));
 app.get("/",(req,res)=>{
     return res.json({
