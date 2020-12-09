63379640b56d01cb450daf2faea46821919f5969
Devilzer <jenadeepak724@gmail.com>
2020-12-09 20:09:39 +0530

    add login and token generation

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,20 +1,65 @@
 const Doctors = require("../models/doctors");
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
+let refreshTokenlist = [];
+
+//doctor sign up meathod
 module.exports.register =async (req,res)=>{
     try {
         // console.log(req.body);
         const doctor = new Doctors;
         doctor.username = req.body.username;
         doctor.password = req.body.password;
-        await doctor.save();
+        const tempdoc =await Doctors.findOne({username:doctor.username,password:doctor.password});
+        if(tempdoc){
+            return res.status(409).json({
+                message : "user already exists please login."
+            });
+        }else{
+            await doctor.save();
         return res.status(200).json({
             message : "user created!"
         });
+        }  
+    } catch (error) {
+        return res.status(400).json({
+            message : `Error in creating user ${error}`
+        });   
+    }  
+};
+
+//doctor sign in meathod using jwt
+module.exports.login = async(req,res)=>{
+    try {
+        const doctor  = await Doctors.findOne({username:req.body.username,password:req.body.password});
+        if(doctor){
+            console.log(doctor);
+            const payload = {
+                username : doctor.username,
+                password : doctor.password
+            };
+            const accessToken = jwt.sign(payload,process.env.ACCESS_TOKEN_SECERT,{
+                expiresIn : "1d"
+            });
+            const refreshToken = jwt.sign(payload,process.env.REFRESH_TOKEN_SECRET,{
+                expiresIn : "1d"
+            });
+            refreshTokenlist.push(refreshToken);
+            return res.status(200).json({
+                message:"login success!!",
+                AccessToken :accessToken,
+                RefreshToken :refreshToken
+            });
+        }
+        else{
+            return res.status(401).json({
+                message:"Invalid username or Password"
+            });
+        }
     } catch (error) {
         return res.status(400).json({
             message : error
         });
-        
-    }  
-
+    }
 };
+
--- routes/api/v1/doctor.js
+++ routes/api/v1/doctor.js
@@ -3,6 +3,7 @@ const router = express();
 const doctorController = require("../../../controllers/doctorController");
 
 router.post("/register",doctorController.register);
+router.post("/login",doctorController.login);
 
 
 module.exports = router;
