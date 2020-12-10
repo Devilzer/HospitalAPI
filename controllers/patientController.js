a9e15d7ba131d5a34ddd1ea50f3a00aa83a8a2c7
Devilzer <jenadeepak724@gmail.com>
2020-12-10 21:08:15 +0530

    add report generate

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,15 +1,90 @@
 const Patients = require("../models/patients");
-
+const Reports = require("../models/reports");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+//method for registering patients to database
 module.exports.register=async(req,res)=>{
     try {
         const patient = new Patients;
-        console.log(req.body);
+        patient.name = req.body.name;
+        patient.phone = req.body.phone;
+        await patient.save();
         return res.status(200).json({
-            message:"pat success"
+            message:"Success patient Created!"
         });
     } catch (error) {
         return res.status(400).json({
             message : `Error in creating patient ${error}`
         });
     }
-}
+};
+//method for creating patient report 
+module.exports.createReport = async(req,res)=>{
+    try {
+        const patient =await Patients.findOne({phone:req.params.phone});
+        if(patient){
+            const authHeader = req.headers["authorization"];
+            const token = authHeader && authHeader.split(" ")[1];
+            const doctor = jwt.verify(token,process.env.ACCESS_TOKEN_SECERT);
+            const report  = new Reports;
+            report.patient_name=patient.name;
+            report.created_by_doc= doctor.username;
+            if(req.body.status=="N"){
+                report.status = "Negative"
+            }
+            else if(req.body.status=="TQ"){
+                report.status = "Travelled-Quarantine"
+            }
+            else if(req.body.status=="SQ"){
+                report.status = "Symptoms-Quarantine"
+            }
+            else if(req.body.status=="PA"){
+                report.status = "Positive-Admit"
+            }
+            await report.save();
+            return res.status(200).json({
+                message : "Report created",
+                report : report
+            });
+
+        }
+        else{
+            return res.status(400).json({
+                message:"error in finding patient"
+            });
+        }
+        // const report = new Reports;
+        // report.patient_name
+        
+        
+
+    } catch (error) {
+        return res.status(400).json({
+            message:`error in creating report ${error}`
+        });
+    }
+}; 
+
+//method to generate all reports
+module.exports.allreports = async(req,res)=>{
+    try {
+        const patient =await Patients.findOne({phone:req.params.phone});
+        if(patient){
+            console.log(patient);
+            return res.status(200).json({
+                message:"report all success"
+            });
+        }
+        else{
+            return res.status(400).json({
+                message:"patient not found"
+            });
+        }
+
+        
+    } catch (error) {
+        return res.status(400).json({
+            message: `error in finding reports ${error}`
+        });
+    }
+};
--- routes/api/v1/patient.js
+++ routes/api/v1/patient.js
@@ -4,4 +4,6 @@ const patientController = require("../../../controllers/patientController");
 const jwtController = require("../../../controllers/jwt");
 
 router.post("/register",jwtController.authenticate,patientController.register);
+router.post("/:phone/create_report",jwtController.authenticate,patientController.createReport);
+router.get("/:phone/all_reports",jwtController.authenticate,patientController.allreports);
 module.exports = router;
